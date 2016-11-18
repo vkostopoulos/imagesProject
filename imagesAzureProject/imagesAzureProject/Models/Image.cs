fd60f342df7038b0b5c6bb60041b5bdc2c497e65
vaggos <vaggos@DESKTOP-AUO0452>
2016-11-18 20:38:14 +0200

    Install NuGet Package : Entity Framework 6.1.3 Add files Image.cs to Models , IImageService.cs to Repositories Create ImageContext.cs to Models for Database Initialization

---
+++ imagesAzureProject/imagesAzureProject/Models/Image.cs
@@ -0,0 +1,25 @@
+﻿namespace imagesAzureProject.Models
+{
+    public class Image {
+        /// <summary>
+        /// The Id of the entity
+        /// </summary>
+        /// 
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the image. It can be different than actual file name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the image
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The path the actual image is stored (normally the blob storage reference)
+        /// </summary>
+        public string ImagePath { get; set; }
+    }
+}
---
+++ imagesAzureProject/imagesAzureProject/Repositories/IImageService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace OrderezeTask
+{
+    /// <summary>
+    /// Service responsible managing all Images operations
+    /// </summary>
+    public interface IImagesService
+    {
+        /// <summary>
+        /// Returns all images 
+        /// </summary>
+        /// <returns></returns>
+        List<Image> GetImages();
+
+        /// <summary>
+        /// Adds the supplied <paramref name="image"/> to the system and returns the Id.
+        /// Part of the operation is to store the Image in the blob storage.
+        /// </summary>
+        int AddNewImage(Image image);
+
+        /// <summary>
+        /// Deletes the Image with the supplied <paramref name="id"/> from the system 
+        /// and deletes the file from the blob storage as well.
+        /// </summary>
+        void DeleteImage(int id);
+    }
+}
